52c28f3c8fde04dfa7885fd1e2751e8cba69ec42
Eduardo Bratz <ebratz@gmail.com>
2021-11-06 16:29:12 -0300

    feat: Usando Typescript

--- reactjs/01-github-explorer/src/components/RepositoryItem.tsx
+++ reactjs/01-github-explorer/src/components/RepositoryItem.tsx
@@ -1,4 +1,12 @@
-export function RepositoryItem(props) {
+interface RepositoryItemProps {
+  repository: {
+    name: string;
+    full_name: string;
+    html_url: string;
+  }
+}
+
+export function RepositoryItem(props: RepositoryItemProps) {
   return(
     <li>
       <strong>{props?.repository.name ?? 'default'}</strong>
--- reactjs/01-github-explorer/src/components/RepositoryList.tsx
+++ reactjs/01-github-explorer/src/components/RepositoryList.tsx
@@ -3,9 +3,14 @@ import { RepositoryItem } from "./RepositoryItem";
 import '../styles/repositories.scss'
 import { useEffect, useState } from "react";
 
+interface Repository {
+  name: string;
+  full_name: string;
+  html_url: string;
+}
 
 export function RepositoryList() {
-  const [repositories, setRepositories] = useState([]);
+  const [repositories, setRepositories] = useState<Repository[]>([]);
 
   useEffect(() => {
     fetch('https://api.github.com/users/ebratz/repos')
