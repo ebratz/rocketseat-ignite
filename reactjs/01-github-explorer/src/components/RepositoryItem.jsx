fd582cdb92bf11b92f836dc7dadf187b71b58d99
Eduardo Bratz <ebratz@gmail.com>
2021-11-06 14:24:46 -0300

    feat: Chamadas HTTP

--- reactjs/01-github-explorer/src/components/RepositoryItem.jsx
+++ reactjs/01-github-explorer/src/components/RepositoryItem.jsx
@@ -1,9 +1,9 @@
 export function RepositoryItem(props) {
   return(
     <li>
-          <strong>{props.repository.name ?? 'default'}</strong>
-          <p>{props.repository.description}</p>
-          <a href={props.repository.link}>Acessar Repositório</a>
-        </li>
+      <strong>{props?.repository.name ?? 'default'}</strong>
+      <p>{props?.repository.full_name}</p>
+      <a href={props?.repository.html_url}>Acessar Repositório</a>
+    </li>
   )
 }
--- reactjs/01-github-explorer/webpack.config.js
+++ reactjs/01-github-explorer/webpack.config.js
@@ -45,7 +45,7 @@ module.exports = {
       {
         test: /\.scss$/,
         exclude: /node_modules/,
-        use: ['style-loader', 'css-loader'],
+        use: ['style-loader', 'css-loader','sass-loader'],
       }
     ]
   }
