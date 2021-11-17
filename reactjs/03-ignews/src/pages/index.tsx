e8110e81f8321bfba22b35a30dbefbffaf4b40e5
Eduardo Bratz <ebratz@gmail.com>
2021-11-16 22:58:30 -0300

    feat: Componentes e Páginas

--- reactjs/03-ignews/src/pages/index.tsx
+++ reactjs/03-ignews/src/pages/index.tsx
@@ -1,15 +1,27 @@
-
+import styles from './home.module.scss'
 import Head from 'next/head'
+import { SubscribeButton } from '../components/SubscribeButton'
 
 export default function Home() {
   return (
     <>
     <Head>
-    <title>Início | ig.news </title>
+    <title>Home | ig.news </title>
     </Head>
 
 
-    <h1>Hello World</h1>
+    <main className={styles.contentContainer}>
+      <section className={styles.hero}>
+        <span>👏 Hey, welcome</span>
+        <h1>News about the <span>React</span> world.</h1>
+        <p>Get access to all publications <br />
+          <span>For only $9.90 month</span>
+        </p>
+        <SubscribeButton />
+      </section>
+
+      <img src="/images/avatar.svg" alt="Girl Coding" />
+      </main>
     </>
   )
 }
