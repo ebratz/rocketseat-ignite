a6ba543a953545e0dd35cd170005ef7c1edb5fab
Eduardo Bratz <ebratz@gmail.com>
2021-11-18 23:07:50 -0300

    feat: Usando FaundaDB

--- reactjs/03-ignews/src/pages/api/auth/[...nextauth].ts
+++ reactjs/03-ignews/src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,7 @@
 import NextAuth from "next-auth"
 import Provider from "next-auth/providers"
+import { fauna } from "../../../services/fauna"
+import { query as q } from "faunadb"
 
 export default NextAuth({
   // Configure one or more authentication providers
@@ -10,4 +12,40 @@ export default NextAuth({
       scope: "read:user"
     }),
   ],
+
+  callbacks: {
+    async signIn (user, account, profile){
+      const { email } = user
+
+      try {
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index("users_by_email"),
+                  q.Casefold(user.email)
+                )
+              )
+            ),
+            q.Create(
+              q.Collection("users"),
+              {data: {email}}
+            ),
+            q.Get(
+              q.Match(
+                q.Index("users_by_email"),
+                q.Casefold(user.email)
+              )
+            )
+          )
+        )
+        return true
+      } catch {
+        return false
+      }
+
+      
+    }
+  }
 })
